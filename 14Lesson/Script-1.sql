ada0c4bee4c811ab83cee2713fec6d7aac21407e
Shima-Creator <shima.228@outlook.com>
2024-11-12 19:15:55 +0300

    Debug sql file

--- 14Lesson/Script-1.sql
+++ 14Lesson/Script-1.sql
@@ -1,31 +1,31 @@
-CREATE DATABASE Employees;
+CREATE DATABASE employees;
 
-CREATE TABLE Employees (Name VARCHAR(50),
+CREATE TABLE employees (name VARCHAR(50),
 						Position VARCHAR(150),
-						Department VARCHAR(120),
-						Salary INT);
+						department VARCHAR(120),
+						salary INT);
 
-INSERT INTO Employees (Name, Position, Department, Salary)
+INSERT INTO employees (name, Position, department, salary)
 VALUES ('Ilya Shimanko', 'developer', 'development', 4000),
 		('Nickolay Efremov', 'manager', 'marketing', 6000),
 		('Alexandr Petrov','salesman','sales',7500);
 		
 
-UPDATE Employees SET Position='main-salesman' WHERE Name='Alexandr Petrov';
+UPDATE employees SET Position='main-salesman' WHERE name='Alexandr Petrov';
 
 alter table employees add column HireDate DATE;
 
-update employees set Hiredate='June 30, 2023' where Name='Alexandr Petrov';
-update employees set Hiredate='July 13, 2021' where Name='Ilya Shimanko';
-update employees set Hiredate='October 22, 2022' where Name='Nickolay Efremov';
+update employees set Hiredate='June 30, 2023' where name='Alexandr Petrov';
+update employees set Hiredate='July 13, 2021' where name='Ilya Shimanko';
+update employees set Hiredate='October 22, 2022' where name='Nickolay Efremov';
 
-select Name from employees where position='manager';
+select name from employees where position='manager';
 
-select Name from employees where Salary>5000;
+select name from employees where salary>5000;
 
-select Name from employees where department ='sales';
+select name from employees where department ='sales';
 
-select AVG(Salary) as AvgSalary from employees;
+select AVG(salary) as AvgSalary from employees;
 
-DROP TABLE Employees;
+DROP TABLE employees;
 
