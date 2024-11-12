c5e4e558fc8498058221f738fd3655867d961155
Shima-Creator <shima.228@outlook.com>
2024-11-12 19:18:34 +0300

    Completed exercise with DB Employees (#5)
    * Completed exercise with DB Employees

---
+++ 14Lesson/Script-1.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE employees;
+
+CREATE TABLE employees (name VARCHAR(50),
+						Position VARCHAR(150),
+						department VARCHAR(120),
+						salary INT);
+
+INSERT INTO employees (name, Position, department, salary)
+VALUES ('Ilya Shimanko', 'developer', 'development', 4000),
+		('Nickolay Efremov', 'manager', 'marketing', 6000),
+		('Alexandr Petrov','salesman','sales',7500);
+		
+
+UPDATE employees SET Position='main-salesman' WHERE name='Alexandr Petrov';
+
+alter table employees add column HireDate DATE;
+
+update employees set Hiredate='June 30, 2023' where name='Alexandr Petrov';
+update employees set Hiredate='July 13, 2021' where name='Ilya Shimanko';
+update employees set Hiredate='October 22, 2022' where name='Nickolay Efremov';
+
+select name from employees where position='manager';
+
+select name from employees where salary>5000;
+
+select name from employees where department ='sales';
+
+select AVG(salary) as AvgSalary from employees;
+
+DROP TABLE employees;
+
