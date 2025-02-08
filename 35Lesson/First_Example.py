5cd068d9fc250a2b5c7c39001c72f69b6b35ed6d
Shima-Creator <shima.228@outlook.com>
2025-02-08 20:06:28 +0300

    Added list of URLS

--- 35Lesson/First_Example.py
+++ 35Lesson/First_Example.py
@@ -23,7 +23,7 @@ spec_symbols_list = ('~', '|', '/', '.', ',', '*', '-',
 def write_file(search_method, start_time, finish_time, password):
     print(f"Время выполнения {search_method} метода - {finish_time - start_time:4f}. Пароль {password}.\n")
 
-    with open("Results.txt", 'a') as result_file:
+    with open("ResultsSearchPassword.txt", 'a') as result_file:
         result_file.write(
             f"Время выполнения {search_method} метода - {finish_time - start_time:4f}. Пароль {password}.\n")
 
