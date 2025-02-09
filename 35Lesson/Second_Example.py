4221518008f9c80d595c0a304991302bdd6eb088
Shima-Creator <shima.228@outlook.com>
2025-02-09 19:40:45 +0300

    Added function for write result to file

--- 35Lesson/Second_Example.py
+++ 35Lesson/Second_Example.py
@@ -29,3 +29,10 @@ URLS = [
         "https://www.cloudflare.com",
         "https://www.ibm.com",
     ]
+
+def write_file(search_method, start_time, finish_time):
+    print(f"Время выполнения {search_method} метода - {finish_time - start_time:4f}.\n")
+
+    with open("ResultsGetRequests.txt", 'a') as result_file:
+        result_file.write(
+            f"Время выполнения {search_method} метода - {finish_time - start_time:4f}.\n")
