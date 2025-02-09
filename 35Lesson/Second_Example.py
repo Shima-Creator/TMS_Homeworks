5ae6de3ca9b1919af9b94a773b00709d912314a7
Shima-Creator <shima.228@outlook.com>
2025-02-09 19:39:54 +0300

    Added list of URLs

---
+++ 35Lesson/Second_Example.py
@@ -0,0 +1,31 @@
+import multiprocessing
+import threading
+
+import requests
+import time
+
+URLS = [
+        "https://www.google.com",
+        "https://www.youtube.com",
+        "https://www.facebook.com",
+        "https://www.instagram.com",
+        "https://www.linkedin.com",
+        "https://www.wikipedia.org",
+        "https://www.microsoft.com",
+        "https://www.apple.com",
+        "https://www.bbc.com",
+        "https://www.nytimes.com",
+        "https://www.cnn.com",
+        "https://www.reddit.com",
+        "https://www.stackoverflow.com",
+        "https://github.com",
+        "https://www.yahoo.com",
+        "https://www.bing.com",
+        "https://www.paypal.com",
+        "https://www.netflix.com",
+        "https://www.office.com",
+        "https://www.salesforce.com",
+        "https://www.adobe.com",
+        "https://www.cloudflare.com",
+        "https://www.ibm.com",
+    ]
